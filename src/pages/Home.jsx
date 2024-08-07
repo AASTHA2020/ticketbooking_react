2c7241baf9d1d5b2bd61016f2ffe35a4fb74b5f5
AASTHA2020 <modiaastha0@gmail.com>
2024-08-07 17:39:26 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,17 +12,17 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.section}>
-        <img src="/src/assets/leftImage.jpg" alt="Left Image" className={styles.image} />
+        <img src="/src/assets/leftImage.jpg" alt="Book Your Tickets Now" className={styles.image} />
         <div className={styles.overlay}>
-          <h2>Left Section</h2>
-          <button onClick={handleNavigate}>Discover More</button>
+          <h2 className={styles.header}>Book Your Tickets Now</h2>
+          <button className={styles.leftButton} onClick={handleNavigate}>Discover More</button>
         </div>
       </div>
       <div className={styles.section}>
-        <img src="/src/assets/rightImage.jpg" alt="Right Image" className={styles.image} />
+        <img src="/src/assets/rightImage.jpg" alt="Secure Your Concert Seats" className={styles.image} />
         <div className={styles.overlay}>
-          <h2>Right Section</h2>
-          <button onClick={handleNavigate}>Discover More</button>
+          <h2 className={styles.header}>Secure Your Concert Seats</h2>
+          <button className={styles.rightButton} onClick={handleNavigate}>Discover More</button>
         </div>
       </div>
     </div>
