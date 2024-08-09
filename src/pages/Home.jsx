c9b6d6e5378d5808008c4f5e0c9ab2d95a91f71b
AASTHA2020 <modiaastha0@gmail.com>
2024-08-09 19:26:46 +0530

    smooth transition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,14 +18,14 @@ const Home = () => {
       <div className={styles.section}>
         <img src={leftImage} alt="Book Your Tickets Now" className={styles.image} />
         <div className={styles.overlay}>
-          <h2 className={styles.header}>BOOK YOUR <b>TICKETS NOW</b></h2>
+          <h2 className={styles.header}>BOOK YOUR <br /> TICKETS NOW</h2>
           <button className={styles.leftButton} onClick={() => handleNavigate('/ticketBooking')}>Discover More</button>
         </div>
       </div>
       <div className={styles.section}>
         <img src={rightImage} alt="Secure Your Concert Seats" className={styles.image} />
         <div className={styles.overlay}>
-          <h2 className={styles.header}>SECURE YOUR <b>CONCERT SEATS</b></h2>
+          <h2 className={styles.header}>SECURE YOUR <br /> CONCERT SEATS</h2>
           <button className={styles.rightButton} onClick={() => handleNavigate('/ticketBooking')}>Discover More</button>
         </div>
       </div>
